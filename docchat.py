dd6f596bbe1634bb7f02059e9f89316fac68cb74
charlottegjordan <char.gracej@gmail.com>
2025-04-26 20:25:44 -0700

    update

--- docchat.py
+++ docchat.py
@@ -1,21 +1,21 @@
 # PRELIM STUFF
 import readline
 import nltk
-nltk.download('punkt')
+
 
 from dotenv import load_dotenv
 load_dotenv()
 
-
+"""
 import argparse
 parser = argparse.ArgumentParser(
-    prog='docsum',
+    prog='docchat',
     description='summarize the input',
     )
 
 parser.add_argument('filename')
 args = parser.parse_args()
-
+"""
 
 # SUPPLEMENTARY FUNCTIONS
 def llm(messages, temperature=1):
@@ -241,7 +241,7 @@ def summarize_text(text):
         summarized_text = summarize_text(summarized_text)
         return summarized_text
 
-
+"""
 #MAIN FUNCTION
 import pprint
 if __name__ == '__main__':
@@ -280,3 +280,4 @@ if __name__ == '__main__':
         })
 
         print(result)
+"""        
